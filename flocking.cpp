4b03c24dab31d2911f21780706c9d1f230517d18
Sean Wu <slwu89@berkeley.edu>
2016-10-01 18:12:17 -0700

    flocking animation

--- flocking.cpp
+++ flocking.cpp
@@ -111,30 +111,106 @@ tmp <- flocking_test(10,0.3,.001,TRUE)
 */
 
 // [[Rcpp::export]]
-List flocking(int n_iter, int n, double safe_dist, double speed, double inertia, bool brownian){
+arma::cube flocking(int n_iter, int n, double safe_dist, double speed, double inertia, bool brownian, bool progress){
   
   //Generate xy initial positions.
-  //xypos will hold the current critter position while
-  //xypos0 will hold the position of the critters the previous time.
+  //xypos will hold the current critter position
   arma::mat xypos(n,2);
   for(int i=0; i<n; i++){
     xypos(i,0) = R::runif(0.0,1.0);
     xypos(i,1) = R::runif(0.0,1.0);
   }
   xypos = xypos - 0.5;
-  arma::mat xypos0 = xypos;
-  
-  arma::mat movement0(n,2,arma::fill::zeros);
-  
+
   //output object to store xypos at each time step
   arma::cube xypos_output(n,2,n_iter+1,arma::fill::zeros);
   xypos_output.slice(0) = xypos; //fill initial positions
   
+  //allocate memory for objects we will use later
+  arma::mat movement0(n,2,arma::fill::zeros);
+  arma::mat distances(n,n);
+  
+  //progress bar
+  Progress p(n_iter,progress);
+  
   //loop over time
   for(int iter=0; iter<n_iter; iter++){
     
+    //check for user abort
+    if(Progress::check_abort()){
+      Rcout << "User abort detected at step: " << iter << ", of: " << n_iter << std::endl;
+      return(xypos_output);
+    }
+    
+    //Calculate each critters distance from each other
+    for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+        distances(i,j) = pow(xypos(i,0) - xypos(j,0),2) + pow(xypos(i,1) - xypos(j,1),2); //euclidean distance
+      }
+    }
+    
+    //Drop those within the safe zone.
+    double max_d = distances.max();
+    for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+        if(distances(i,j) < safe_dist || distances(i,j) == 0.0){
+          distances(i,j) = max_d;
+        }
+      }
+    }
+    
+    //This selects the critter closest to the selected critter.
+    arma::mat closest_index(n,n);
+    for(int i=0; i<n; i++){
+      arma::uvec order_i = arma::sort_index(distances.row(i));
+      closest_index.row(i) = arma::conv_to<arma::rowvec>::from(order_i);
+    }
+    arma::vec closest = closest_index.col(0);
+    arma::mat xypos_sort(n,2);
+    for(int i=0; i<n; i++){
+      xypos_sort.row(i) = xypos.row(closest(i));
+    }
+    
+    //This calculates the difference between the current position of each critter and that of the closest critter.
+    arma::mat ab = xypos - xypos_sort;
+    
+    //Now calculate the difference in the horizontal and vertical axes that the critters will move as a projection into the direction of the closest critter outside of the safe zone.
+    arma::vec a_prime = speed / sqrt(1 + (pow(ab.col(1),2)) / (pow(ab.col(0),2)));
+    arma::vec b_prime = sqrt(pow(speed,2) - pow(a_prime,2));
+    
+    //This corrects the movement to ensure that the critters are flying at each other rather than away from each other.
+    arma::mat movement(n,2);
+    movement.col(0) = a_prime % arma::sign(ab.col(1));
+    movement.col(1) = b_prime % arma::sign(ab.col(0));
+    arma::mat between_movement = between_cpp(xypos,xypos_sort,xypos-movement);
+    movement =  element_power_cpp(movement*(-1),between_movement);
+    
+    //MIGHT NEED TO CHECK FOR NaN VALUES IN MOVEMENT?
+    
+    movement0 = movement0*inertia + movement;
+    
+    //This fancy dodad allows half of the change in movement to be due to random variation.
+    if(brownian){
+      arma::mat brownian_motion(n,2);
+      for(int i=0; i<n; i++){
+        brownian_motion(i,0) = R::runif(0.0,1.0);
+        brownian_motion(i,1) = R::runif(0.0,1.0);
+      }
+      movement0 = movement0 + brownian_motion * speed/2;
+    }
+    
+    //Update the current round's.
+    xypos = xypos + movement0;
+    
+    //push output
+    xypos_output.slice(iter+1) = xypos;
+    
+    //advance progress bar
+    p.increment();
   }
   
+  //return output
+  return(xypos_output);
 }
 
 
